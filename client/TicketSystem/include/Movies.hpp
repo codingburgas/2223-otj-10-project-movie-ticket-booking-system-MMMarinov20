83e0edc14ceb1c6a30d2e2bdbe1bf57ddf2d03dd
Maxim Marinov <MMMarinov20@codingburgas.bg>
2023-05-08 16:14:32 +0300

    feat: fetch movies api and extract the title

--- client/TicketSystem/include/Movies.hpp
+++ client/TicketSystem/include/Movies.hpp
@@ -1,6 +1,12 @@
 #pragma once
 #include <string>
 #include <iostream>
+#include <array>
+#include "rapidjson/document.h"
+#include "rapidjson/writer.h"
+#include "rapidjson/stringbuffer.h"
+#include "rapidjson/istreamwrapper.h"
+#include "rapidjson/error/en.h"
 #using <System.dll>
 
 using namespace System;
@@ -11,6 +17,8 @@ using namespace System::Data;
 using namespace System::Drawing;
 using namespace System::Net;
 using namespace System::Text;
+using namespace System::Runtime::InteropServices;
+using namespace System::IO;
 
 public ref class Movies : public System::Windows::Forms::Form {
 public:
@@ -19,19 +27,32 @@ public:
 	}
 	void fetchMovies();
 	String^ getMovies();
+	//create array of type String^
+	
+	cli::array<String^>^ movies = gcnew cli::array<String^>(100);
 
 	private: System::Windows::Forms::Label^ movieName;
 
 	void InitializeComponent(void) {
 		this->movieName = (gcnew System::Windows::Forms::Label());
 		this->SuspendLayout();
-		
+
+		this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
+		this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+		this->BackColor = System::Drawing::Color::IndianRed;
+		this->ClientSize = System::Drawing::Size(1264, 681);
+		this->Name = L"WelcomeScreen";
+		this->Text = L"WelcomeScreen";
+		this->ResumeLayout(false);
+		this->PerformLayout();
+
+		fetchMovies();
+		this->movieName->Text = movies[0];
+		this->Controls->Add(this->movieName);
+		this->movieName->Location = System::Drawing::Point(100, 100);
 		this->movieName->AutoSize = true;
-		this->movieName->Location = System::Drawing::Point(0, 100);
 		this->movieName->Name = L"movieName";
 		this->movieName->Size = System::Drawing::Size(35, 13);
-		this->movieName->Text = L"label1";
-		this->Controls->Add(this->movieName);
 	}
 private:
 	String^ movieData;
