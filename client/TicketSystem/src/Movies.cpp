d1cc930f025709f069315bcd88bb56278de99923
Maxim Marinov <MMMarinov20@codingburgas.bg>
2023-05-09 10:54:02 +0300

    fix: fix missing file

--- client/TicketSystem/src/Movies.cpp
+++ client/TicketSystem/src/Movies.cpp
@@ -3,23 +3,74 @@
 
 using namespace std;
 void Movies::fetchMovies() {
-	String^ url = "https://api.themoviedb.org/3/movie/now_playing?api_key=1f54bd990f1cdfb230adb312546d765d";
+	// Create WebClient to make HTTP request
 	WebClient^ client = gcnew WebClient();
-	String^ json = Encoding::UTF8->GetString(client->DownloadData(url));
 
-    int length = json->Length;
-    char* buffer = new char[length + 1];
+	// Set API endpoint URL and headers
+	Uri^ url = gcnew Uri("https://moviesdatabase.p.rapidapi.com/titles/?year=2023");
+	client->Headers->Add("X-RapidAPI-Key", "695f25458cmsh1b721beeecf3cf3p18466ejsn05afc1a3635f");
+	client->Headers->Add("X-RapidAPI-Host", "moviesdatabase.p.rapidapi.com");
 
-    for (int i = 0; i < length; i++)
-    {
-        buffer[i] = static_cast<char>(json[i]);
-    }
-    buffer[length] = '\0';
+	try
+	{
+		Stream^ stream = client->OpenRead(url);
+		StreamReader^ reader = gcnew StreamReader(stream);
+		String^ json = reader->ReadToEnd();
 
-    std::string str(buffer); // Convert from char* to std::string
-    delete[] buffer;
-    this->movieData = json;
-	Console::WriteLine(json);
+		char* ch = static_cast<char*>(Marshal::StringToHGlobalAnsi(json).ToPointer());
+
+		rapidjson::Document doc;
+		doc.Parse(ch);
+
+		Marshal::FreeHGlobal(IntPtr(ch));
+
+		if (doc.IsObject())
+		{
+			// Access the "results" array
+			if (doc.HasMember("results") && doc["results"].IsArray())
+			{
+				for (rapidjson::SizeType i = 0; i < doc["results"].Size(); i++)
+				{
+					const rapidjson::Value& result = doc["results"][i];
+
+					if (result.HasMember("titleText") && result["titleText"].IsObject())
+					{
+						const rapidjson::Value& titleText = result["titleText"];
+						if (titleText.HasMember("text") && titleText["text"].IsString())
+						{
+							String^ title = gcnew String(titleText["text"].GetString());
+							String^ url = "";
+							int year = 0;
+							String^ caption = "";
+							String^ type = "";
+
+							if (result.HasMember("primaryImage") && result["primaryImage"].IsObject())
+							{
+								caption = gcnew String(result["primaryImage"]["caption"]["plainText"].GetString());
+								url = gcnew String(result["primaryImage"]["url"].GetString());
+							}
+
+							if (result.HasMember("releaseYear") && result["releaseYear"].IsObject())
+							{
+								year = result["releaseYear"]["year"].GetInt();
+
+								if (result.HasMember("type") && result["type"].IsObject()) type = gcnew String(result["type"].GetString());
+							}
+							this->movies[i] = title;
+							this->captions[i] = caption;
+							this->url[i] = url;
+							this->years[i] = year.ToString();
+						}
+					}
+				}
+			}
+		}
+	}
+	catch (WebException^ e)
+	{
+		// Handle any exceptions thrown by the WebClient
+		Console::WriteLine("WebException: {0}", e->Message);
+	}
 }
 
 String^ Movies::getMovies() {
