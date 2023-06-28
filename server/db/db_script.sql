b35755a1b77e3ff2260c2cf37ae52357e9d5970f
Maxim Marinov <MMMarinov20@codingburgas.bg>
2023-06-28 10:37:30 +0300

    chore: add db script

---
+++ server/db/db_script.sql
@@ -0,0 +1,9 @@
+DROP DATABASE IF EXISTS `tickets`;
+CREATE DATABASE `tickets`;
+
+USE `tickets`;
+CREATE TABLE `tickets` (
+    `uuid` VARCHAR(36) NOT NULL,
+    `seat_number` INT NOT NULL,
+    `purchase_date` DATE NOT NULL, 
+)
