020eaff7e6626bff8f52b58b4e51e963c91a8cab
Maxim Marinov <MMMarinov20@codingburgas.bg>
2023-06-28 19:31:38 +0300

    chore: add DB script

--- server/db/db_script.sql
+++ server/db/db_script.sql
@@ -5,5 +5,5 @@ USE `tickets`;
 CREATE TABLE `tickets` (
     `uuid` VARCHAR(36) NOT NULL,
     `seat_number` INT NOT NULL,
-    `purchase_date` DATE NOT NULL, 
-)
+    `purchase_date` DATE NOT NULL
+);
