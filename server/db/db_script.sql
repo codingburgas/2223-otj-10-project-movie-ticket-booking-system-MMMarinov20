1dae3892fbe1ace58f80335f74b21c1a5f9f14dc
Maxim Marinov <MMMarinov20@codingburgas.bg>
2023-06-28 19:54:26 +0300

    feat: create /postTickets endpoint

--- server/db/db_script.sql
+++ server/db/db_script.sql
@@ -5,5 +5,7 @@ USE `tickets`;
 CREATE TABLE `tickets` (
     `uuid` VARCHAR(36) NOT NULL,
     `seat_number` INT NOT NULL,
+    `movie_name` VARCHAR(255) NOT NULL,
+    `movie_time` VARCHAR(255) NOT NULL,
     `purchase_date` DATE NOT NULL
 );
--- server/routes/api.js
+++ server/routes/api.js
@@ -11,9 +11,20 @@ router.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-router.get("/tickets", (req, res) => {
+router.get("/getTickets", (req, res) => {
   db.query("SELECT * FROM tickets", (err, result) => {
     if (err) throw err;
     res.send(result);
+  });
+});
+
+router.post("/postTicket", (req, res) => {
+  const id = uuid.v4();
+  const { seat_number, movie_name, movie_time } = req.body;
+  const purchase_date = new Date().toISOString().slice(0, 19).replace("T", " ");
+  const sql = `INSERT INTO tickets (uuid, seat_number, movie_name, movie_time, purchase_date) VALUES ("${id}", "${seat_number}", "${movie_name}", "${movie_time}", "${purchase_date}")`;
+  db.query(sql, (err, result) => {
+    if (err) throw err;
+    res.send("Ticket successfully purchased!");
   });
 });
